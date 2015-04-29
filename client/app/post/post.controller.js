3ab415c8990a928608399e18910980dcccee0e11
Zoracka <atzoracka@gmail.com>
2015-04-29 12:30:47 +0200

    post service and ng-repeat posts in post-view

--- client/app/post/post.controller.js
+++ client/app/post/post.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('favorForFreeApp')
-  .controller('PostCtrl', function ($scope) {
+  .controller('PostCtrl', function ($scope, postService) {
     $scope.message = 'Hello';
+
+    $scope.posts = [];
+    
+    postService.list(function(posts){
+        $scope.posts = posts;
+    });
   });
