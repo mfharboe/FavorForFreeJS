c7b530ed8e4553a2c603e0c5ae38a5ec70a8bb86
SpamHam <peterthraneolsen@gmail.com>
2015-04-30 10:53:16 +0200

    post service added

--- client/app/post/post.service.js
+++ client/app/post/post.service.js
@@ -1,11 +1,35 @@
 'use strict' 
 
 angular.module('favorForFreeApp')
-    .factory('postService', function($http){
-    var service = {};
-    service.list = function (callback){
-        $http.get('/api/posts').success(callback);
-    };
+.factory('postsService',function($http){
+			var service = {};
+			service.list = function(callback){
+				$http.get('/api/posts').success(callback);
+			};
 
-    return service;
-});
+			service.delete = function(_id, callback){ 	
+				$http.delete('/api/posts/' + _id).
+				success(function(data, status, headers, config) {
+					service.list(callback);
+				});
+			};
+	
+			service.find = function(_id, callback){ 	
+				$http.get('/api/posts/'+_id).success(callback);
+			};
+    
+            service.create = function(post, callback){
+            $http.post('/api/posts/', post).success(function(data, status, headers, config){
+            service.list(callback);
+            });
+            };
+    
+            service.createComment = function(id, comment, callback){
+            $http.post('api/posts/' + id + '/comments', comment).success(function(data, status, headers, config){
+            service.find(id, callback);
+            });
+            };
+    
+    
+			return service;
+  });
