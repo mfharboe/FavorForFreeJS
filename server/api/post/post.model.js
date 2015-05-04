44b6cfec9ed7f083e6a8ecc2fa57b9470461d90c
mipe85 <mich89g5@easv365.dk>
2015-05-04 12:29:27 +0200

    post view work

--- server/api/post/post.model.js
+++ server/api/post/post.model.js
@@ -6,16 +6,13 @@ var mongoose = require('mongoose'),
 
 var PostSchema = new Schema({
     title: String,
-    description: {type: String, maxLength: 160},
     authorId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}, //Reference to the user ID only.
     authorName: String, //Name of the user who posted it.
     date: {type: Date, default: Date.now}, //gets the current date.
     isHelper: Boolean, //Determines if the user is needing help or wants to help. Set true if the user wants to help, false if the user needs help
     isOpen: Boolean, //Is the post closed
-    postal: String,
-    // GPS_Latitude: Float
-    // GPS_Longtitude: Float
-    body: String,
+    zip: String,
+    description: String,
     participants: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}], //Users who have participated in the comments
     comments: [postComment.schema] //Array of comments for the post
 });
