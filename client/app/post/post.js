b2a310537ad32f53850c90220d7060b764e154b4
mipe85 <mich89g5@easv365.dk>
2015-05-04 13:33:07 +0200

    post view work

--- client/app/post/post.js
+++ client/app/post/post.js
@@ -3,9 +3,14 @@
 angular.module('favorForFreeApp')
   .config(function ($stateProvider) {
     $stateProvider
-      .state('post', {
-        url: '/post',
-        templateUrl: 'app/post/post.html',
+      .state('favors-given', {
+        url: '/favors-given',
+        templateUrl: 'app/post/favors-given.html',
+        controller: 'PostCtrl'
+      })
+      .state('favors-needed', {
+        url: '/favors-needed',
+        templateUrl: 'app/post/favors-needed.html',
         controller: 'PostCtrl'
       })
       .state('comment', {
