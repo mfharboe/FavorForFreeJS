744d194e97e5d0ba789a8fa62148260e832bf4f2
Zoracka <atzoracka@gmail.com>
2015-05-04 12:07:08 +0200

    index file

---
+++ client/app/profile/profile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('favorForFreeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'app/profile/profile.html',
+        controller: 'ProfileCtrl'
+      });
+  });
