b739251b185854d6562eba59fde3d233c1aa15a0
SpamHam <peterthraneolsen@gmail.com>
2015-05-04 11:51:42 +0200

    auth added to post and commentss

--- server/api/post/index.js
+++ server/api/post/index.js
@@ -6,16 +6,16 @@ var commentController = require('./post.comment.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/',auth.isAuthenticated(), controller.index);
+router.get('/:id',auth.isAuthenticated(), controller.show);
+router.post('/',auth.isAuthenticated(), controller.create);
+router.put('/:id',auth.isAuthenticated(), controller.update);
+router.patch('/:id',auth.isAuthenticated(), controller.update);
+router.delete('/:id',auth.isAuthenticated(), controller.destroy);
 
-router.post('/:id/comments', commentController.create); //Route for creating a new comment. The route requires a post-id.
-router.put('/:id/comments/:cid', commentController.update); //Route for updating an existing comment. The route requires a post id and a comment id.
-router.patch('/:id/comments/:cid', commentController.update); // Same as above
-router.delete('/:id/comments/:cid', commentController.delete); // Route for deleting a comment. The route requires a post id and a comment id.
+router.post('/:id/comments',auth.isAuthenticated(), commentController.create); //Route for creating a new comment. The route requires a post-id.
+router.put('/:id/comments/:cid',auth.isAuthenticated(), commentController.update); //Route for updating an existing comment. The route requires a post id and a comment id.
+router.patch('/:id/comments/:cid',auth.isAuthenticated(), commentController.update); // Same as above
+router.delete('/:id/comments/:cid',auth.isAuthenticated(), commentController.delete); // Route for deleting a comment. The route requires a post id and a comment id.
 
 module.exports = router;
