7b1093325c86163a5d045b2e42c62c0a6c828355
SpamHam <peterthraneolsen@gmail.com>
2015-05-04 13:57:18 +0200

    post en post

--- client/app/post/post.controller.js
+++ client/app/post/post.controller.js
@@ -12,4 +12,22 @@ angular.module('favorForFreeApp')
         $scope.details = function (_id) {
             $state.go('comment', {id: _id})
         };
-    });
+
+    /**
+    todo list/ mangler til addPost:
+    authorId
+    authorName
+    isHelper
+    isOpen
+    */
+  $scope.addPost = function(){
+ if(!$scope.title || $scope.title === '') { return; }
+  console.log('addPost --------!!!!!!!!!!!!');
+  postService.create({title: $scope.title, description: $scope.description, zip: $scope.zip}, function(posts){
+  $scope.posts = posts;
+  });
+        
+  $scope.title = '';
+  
+  };
+});
