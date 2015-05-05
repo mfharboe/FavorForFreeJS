2cd501b1c04197e61da81d59b738dec7b3417098
mfharboe <mfharboe@hotmail.com>
2015-05-05 12:38:11 +0200

    fixed issues regarding newPost

--- server/api/post/post.controller.js
+++ server/api/post/post.controller.js
@@ -4,51 +4,77 @@ var _ = require('lodash');
 var Post = require('./post.model');
 
 // Get list of posts
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   Post.find(function (err, posts) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(200, posts);
   });
 };
 
 // Get a single post
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   Post.findById(req.params.id, function (err, post) {
-    if(err) { return handleError(res, err); }
-    if(!post) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!post) {
+      return res.send(404);
+    }
     return res.json(post);
   });
 };
 
 // Creates a new post in the DB.
-exports.create = function(req, res) {
-  Post.create(req.body, function(err, post) {
-    if(err) { return handleError(res, err); }
+exports.create = function (req, res) {
+  var newPost = req.body;
+  newPost.authorName = req.user.name;
+  newPost.authorId = req.user._id;
+  newPost.isOpen = true;
+  Post.create(newPost, function (err, post) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(201, post);
   });
 };
 
 // Updates an existing post in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   Post.findById(req.params.id, function (err, post) {
-    if (err) { return handleError(res, err); }
-    if(!post) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!post) {
+      return res.send(404);
+    }
     var updated = _.merge(post, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, post);
     });
   });
 };
 
 // Deletes a post from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   Post.findById(req.params.id, function (err, post) {
-    if(err) { return handleError(res, err); }
-    if(!post) { return res.send(404); }
-    post.remove(function(err) {
-      if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!post) {
+      return res.send(404);
+    }
+    post.remove(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.send(204);
     });
   });
