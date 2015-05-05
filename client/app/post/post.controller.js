61561fa07eb78064daf20930948a16ca6cf859e6
mipe85 <mich89g5@easv365.dk>
2015-05-05 19:14:20 +0200

    small changes

--- client/app/post/post.controller.js
+++ client/app/post/post.controller.js
@@ -14,27 +14,27 @@ angular.module('favorForFreeApp')
                 id: _id
             })
         };
-
-        /**
-        todo list/ mangler til addPost:
-        authorId
-        authorName
-        isHelper
-        isOpen
-        */
-        $scope.addPost = function () {
+    
+        $scope.addPost = function (isHelper) {
             if (!$scope.title || $scope.title === '') {
                 return;
             }
-            
-            console.log('addPost --------!!!!!!!!!!!!');
+            if (!$scope.description || $scope.description === '') {
+                return;
+            }
+            if (!$scope.zip || $scope.zip === '') {
+                return;
+            }
             postService.create({
                 title: $scope.title,
                 description: $scope.description,
-                zip: $scope.zip
+                zip: $scope.zip,
+                isHelper: isHelper
             }, function (posts) {
                 $scope.posts = posts;
             });
             $scope.title = '';
+            $scope.description = '';
+            $scope.zip = '';
         };
     });
