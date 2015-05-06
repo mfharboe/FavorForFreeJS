70507354ec45c54ae98627daa857c84164148175
mfharboe <mfharboe@hotmail.com>
2015-05-06 16:34:12 +0200

    made nice adjustments to show profile

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -3,12 +3,18 @@
 angular.module('favorForFreeApp')
   .controller('ProfileCtrl', function ($scope, profileService, $stateParams, Auth) {
     $scope.profile = 'undefined';
+    $scope.sex = '';
 
 
 
     profileService.find($stateParams.id, function (profile) {
       console.log(profile.image);
       $scope.profile = profile;
+      if (profile.sex == 0) {
+        $scope.sex = 'male'
+      } else if (profile.sex == 1) {
+        $scope.sex = 'female'
+      };
 
 
 
